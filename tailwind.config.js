8b6fe5316f8d6edc5f57954887c8547ebdbddd22
web-services-pro <106777603+web-services-pro@users.noreply.github.com>
2026-07-07 07:23:10 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: '#0b0f19',
+        darkCard: '#111827',
+        brandOrange: '#ff6b00',
+        brandOrangeHover: '#e05e00',
+      }
+    }
+  },
+  plugins: [],
+}
